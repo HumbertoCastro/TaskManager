04677fe06517cd52076c0e5c951af9ebef4b5725
humbertoCastro <dedebarbos@hotmail.com>
2022-03-03 08:41:58 -0300

    deployment test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 function Header () {
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-  var yyyy = today.getFullYear();
-  today = dd + '/' + mm + '/' + yyyy;
+  let data = new Date();
+  const week = ['Monday', 'Tuesday', 'Wednesday', 'Trursday', 'Friday'];
+  const dd = String(data.getDate()).padStart(2, '0');
+  const mm = String(data.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = data.getFullYear();
+  const today = dd + '/' + mm + '/' + yyyy;
+  let dayName = '';
+  if (data.getDay() > 5 ) {
+    dayName = 'Weekend';
+  } else {
+    dayName = week[data.getDay() -1 ];
+  }
   return (
     <header className="center flex-column">
       <div className="flex-row">
         <span className="grey">{'<h1>'}</span><h1 className="text">NOME DO APP</h1><span className="grey">{'<h1>'}</span>
+      </div>
+      <div className="flex-row">
+        <span className="grey">{'<h1>'}</span><h1 className="text">{ dayName }</h1><span className="grey">{'<h1>'}</span>
       </div>
       <h2 className="grey">{today}</h2>
     </header>
